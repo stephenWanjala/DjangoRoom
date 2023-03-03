e18928cb274c78ac2854b5767c9f01366ea6e091
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-03 23:38:40 +0300

    create a room model

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django_room
+##### Learning Django 
--- base/views.py
+++ base/views.py
@@ -1,22 +1,23 @@
 from django.shortcuts import render
 
-rooms = [
-    {'id': 1, 'name': "Python"},
-    {'id': 2, 'name': "Design with me"},
-    {'id': 3, 'name': "Core draw beginners"},
+from .models import Room
 
-]
+
+# rooms = [
+#     {'id': 1, 'name': "Python"},
+#     {'id': 2, 'name': "Design with me"},
+#     {'id': 3, 'name': "Core draw beginners"},
+#
+# ]
 
 
 # Create your views here.
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, template_name="base/home.html", context=context)
 
 
 def room(request, pk):
-    context_room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            context_room = i
+    context_room = Room.objects.get(id=pk)
     return render(request, template_name="base/room.html", context={'room': context_room})
