04953d29062f5eda44e1b31b5618f708c743b8ac
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-04 02:48:04 +0300

    search rooms

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
 from .forms import RoomForm
-from .models import Room
+from .models import Room, Topic
 
 
 # rooms = [
@@ -14,8 +15,13 @@ from .models import Room
 
 # Create your views here.
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | Q(name__icontains=q)
+        | Q(description__icontains=q))
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics,'room_count':room_count}
     return render(request, template_name="base/home.html", context=context)
 
 
