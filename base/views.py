3f428cbaeeaa1f559ffe6f46f6d2ff3fe7e8eeb7
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-03 23:04:48 +0300

    style 2

--- base/views.py
+++ base/views.py
@@ -11,8 +11,12 @@ rooms = [
 # Create your views here.
 def home(request):
     context = {'rooms': rooms}
-    return render(request, template_name="home.html", context=context)
+    return render(request, template_name="base/home.html", context=context)
 
 
-def room(request):
-    return render(request, template_name="room.html", )
+def room(request, pk):
+    context_room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            context_room = i
+    return render(request, template_name="base/room.html", context={'room': context_room})
