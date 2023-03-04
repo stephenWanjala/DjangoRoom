ee6fa7a28f38370e062369d23802c5b7ed9eb3a6
wanjala stephen <stephenwanjala145@gmail.com>
2023-03-05 01:58:07 +0300

    correct user to update or delete room

--- base/views.py
+++ base/views.py
@@ -18,6 +18,8 @@ from .models import Room, Topic
 # ]
 
 def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
